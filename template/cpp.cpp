a1e703c992fad52a77faf7d5112a939918badcf2
clarinet758 <clarinet758@gmail.com>
2015-09-06 02:12:05 +0900

    template

---
+++ template/cpp.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+#include<vector>
+#include<list>
+#include<stack>
+#include<queue>
+#include<algorithm>
+using namespace std;
+
+
+int main(){
+    int n;
+    scanf("%d",&n);
+    printf("%d\n",n);
+    return 0;
+}
