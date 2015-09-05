10511cc1cbf1f591bc59f18097f36637022eb5c3
nyon <clarinet758@gmail.com>
2015-09-06 02:31:43 +0900

    neru

---
+++ round/round0001-0025/round0001/a.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+import time
+import sys
+import io
+import re
+import math
+import itertools
+import collections
+#sys.stdin=file('input.txt')
+#sys.stdout=file('output.txt','w')
+#10**9+7
+mod=1000000007
+#mod=1777777777
+pi=3.141592653589
+xy=[(1,0),(-1,0),(0,1),(0,-1)]
+bs=[(-1,-1),(-1,1),(1,1),(1,-1)]
+#start = time.clock()
+n,m,a=map(int,raw_input().split())
+print ((n+a-1)/a)*((m+a-1)/a)
+#end = time.clock()
+#print end - start
