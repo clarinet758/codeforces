70595313398b2b136dd950bb800e87f0064eb630
nyon <clarinet758@gmail.com>
2016-03-14 14:20:41 +0900

    save1

--- round/round0301-0325/round0318/b1.py
+++ round/round0301-0325/round0318/b1.py
@@ -27,46 +27,19 @@ ans=chk=0
 #end = time.clock()
 #print end - start
 
-class UnionFind:
-    def __init__(self,size):
-        self.rank=[0]*size
-        self.par=range(size)
-        self.g_num=size
 
-    def find(self, x):
-        if x==self.par[x]:
-            return x
-        self.par[x]=self.find(self.par[x])
-        return self.par[x]
-
-    def same(self,x,y):
-        return self.find(x)==self.find(y)
-
-    def unite(self,x,y):
-        x,y=self.find(x),self.find(y)
-        if x==y:
-            return
-        self.g_num-=1
-        if self.rank[x]>self.rank[y]:
-            self.par[y]=x
-        else:
-            self.par[x]=y
-            if self.rank[x]==self.rank[y]:
-                self.rank[y]+=1
-
-    def group_num(self):
-        return self.g_num
-
-def sol(i,uni):
-    #print i,uni
+def sol(i,uni,ans):
     tansaku=uni[i[0]]+uni[i[1]]
+
     tmp=set([x for x in tansaku if tansaku.count(x)>1])
     if len(tmp)==0:
-        return 0
+        return -1
     kaeshi=-1
     for j in tmp:
+        if ans!=-1 and len(uni[i[0]])+len(uni[i[1]])>=ans+4:
+            break
         if kaeshi==-1:
-            kaeshi=len(uni[i[0]])+len(uni[i[1]])+len(uni[j])-6
+            kaeshi=(len(uni[i[0]])+len(uni[i[1]])+len(uni[j])-6)
         else:
             kaeshi=min(kaeshi,len(uni[i[0]])+len(uni[i[1]])+len(uni[j])-6)
     return kaeshi
@@ -74,13 +47,9 @@ def sol(i,uni):
 N,Q=map(int,raw_input().split())
 l=[]
 uni={}
-hen=[0]*N
-t=range(N)
 for i in range(Q):
     a,b=map(int,raw_input().split())
     l.append((min(a-1,b-1),max(a-1,b-1)))
-    hen[a-1]+=1
-    hen[b-1]+=1
     if uni.has_key(a-1):
         uni[a-1].append(b-1)
     else:
@@ -91,56 +60,11 @@ for i in range(Q):
         uni[b-1]=[a-1]
 ans-=1
 l.sort()
-print l,uni
-exit()
 for i in l:
-    m=sol(i,uni)
-    if m!=0:
+    m=sol(i,uni,ans)
+    if m!=-1 and ans==-1:
+        ans=m
+    elif m!=-1:
         ans=min(ans,m)
 print ans
-exit()
 
-for i in range(0,Q-2):
-    for j in range(i+1,Q-1):
-        if l[i][0]!=l[j][0] and l[i][0]!=l[j][1]:
-            continue
-        elif l[i][1]<l[j][0]:
-            break
-        for k in range(j+1,Q):
-            if l[i][1]<l[k][0]:
-                break
-            tmp=list(set([l[i][0],l[i][1],l[j][0],l[j][1],l[k][0],l[k][1]]))
-            if len(tmp)==3 and ans==-1:
-                ans=hen[tmp[0]]+hen[tmp[1]]+hen[tmp[2]]-6
-            elif len(tmp)==3:
-                ans=min(ans,hen[tmp[0]]+hen[tmp[1]]+hen[tmp[2]]-6)
-print ans
-exit()
-
-
-memo=[0]*N
-uf=UnionFind(N)
-for loop in xrange(Q):
-    a,b=map(int,raw_input().split())
-    uf.unite(a-1,b-1)
-    hen[a-1]+=1
-    memo[a-1]+=1
-
-chk=[0]*N
-for i in range(N):
-    for j in range(N):
-        if uf.same(i,j):
-            chk[i]+=1
-
-for i in range(N-1,-1,-1):
-    for j in range(N):
-        if i!=j and uf.same(i,j):
-            hen[j]+=memo[i]
-            memo[i]=0
-ans=-1
-for i in range(N):
-    if hen[i]==chk[i] and chk[i]>=3 and ans==-1:
-        ans=chk[i]
-    elif hen[i]==chk[i] and chk[i]>=3:
-        ans=min(ans,chk[i])
-    
