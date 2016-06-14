edf8ce416d27e0b75ea521ddd94f8dd1fadf010a
nyon <clarinet758@gmail.com>
2016-06-15 07:08:04 +0900

    yokuasa

---
+++ round/round0351-0375/round0356/round0357/b1.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+import time
+import sys
+import io
+import re
+import math
+import itertools
+import collections
+import bisect
+#sys.stdin=file('input.txt')
+#sys.stdout=file('output.txt','w')
+#10**9+7
+mod=1000000007
+#mod=1777777777
+pi=3.141592653589
+IS=float('inf')
+xy=[(1,0),(-1,0),(0,1),(0,-1)]
+bs=[(-1,-1),(-1,1),(1,1),(1,-1)]
+def gcd(a,b): return a if b==0 else gcd(b,a%b)
+def lcm(a,b): return a*b/gcd(a,b)
+def euclid_dis(x1,y1,x2,y2): return ((x1-x2)**2+(y1-y2)**2)**0.5
+def choco(xa,ya,xb,yb,xc,yc,xd,yd): return 1 if abs((yb-ya)*(yd-yc)+(xb-xa)*(xd-xc))<1.e-10 else 0
+
+n=int(raw_input())
+#n,k=map(int,raw_input().split())
+#l=map(int,raw_input().split())
+s109=10**9
+#n=s109
+a,b,c=1234567,123456,1234
+three=a+b+c
+ans=chk=0
+if n<three:
+    print 'NO'
+    exit()
+tmp=n-three
+#print tmp
+for i in range(809):
+    btmp=tmp-i*a
+    brtmp=btmp/b
+    for j in range(brtmp+1):
+        ctmp=btmp-b*j
+        if ctmp>=0 and ctmp%c==0:
+            print 'YES'
+#            print i,j
+            exit()
+print 'NO'
+#end = time.clock()
+#print end - start
