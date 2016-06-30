9e5e0bd752e09473978f36e2cb7b5fbb1086107b
nyon <clarinet758@gmail.com>
2016-07-01 01:36:06 +0900

    コンテストのもの

---
+++ round/round0351-0375/round0360/a1.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+import time
+import sys
+import io
+import re
+import math
+import itertools
+import collections
+import bisect
+#sys.stdin=file('input.txt')
+#sys.stdout=file('output.txt','w')
+#10**9+7
+mod=1000000007
+#mod=1777777777
+pi=3.141592653589
+IS=float('inf')
+xy=[(1,0),(-1,0),(0,1),(0,-1)]
+bs=[(-1,-1),(-1,1),(1,1),(1,-1)]
+def gcd(a,b): return a if b==0 else gcd(b,a%b)
+def lcm(a,b): return a*b/gcd(a,b)
+def euclid_dis(x1,y1,x2,y2): return ((x1-x2)**2+(y1-y2)**2)**0.5
+def choco(xa,ya,xb,yb,xc,yc,xd,yd): return 1 if abs((yb-ya)*(yd-yc)+(xb-xa)*(xd-xc))<1.e-10 else 0
+
+#n=int(raw_input())
+ans=chk=0
+n,d=map(int,raw_input().split())
+for i in range(d):
+    s=raw_input()
+    if '0' in s:
+        chk+=1
+    else:
+        ans=max(ans,chk)
+        chk=0
+print max(ans,chk)
+#l=map(int,raw_input().split())
+#end = time.clock()
+#print end - start
