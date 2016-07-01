c2dd6a1133451ea67db4ad6454e896c6ebf2a338
nyon <clarinet758@gmail.com>
2016-07-01 23:46:23 +0900

    AはAC、BはTLE

---
+++ round/round0326-0350/round0327/b1.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+import time
+import sys
+import io
+import re
+import math
+import itertools
+import collections
+import bisect
+#sys.stdin=file('input.txt')
+#sys.stdout=file('output.txt','w')
+#10**9+7
+mod=1000000007
+#mod=1777777777
+pi=3.141592653589
+IS=float('inf')
+xy=[(1,0),(-1,0),(0,1),(0,-1)]
+bs=[(-1,-1),(-1,1),(1,1),(1,-1)]
+def gcd(a,b): return a if b==0 else gcd(b,a%b)
+def lcm(a,b): return a*b/gcd(a,b)
+def euclid_dis(x1,y1,x2,y2): return ((x1-x2)**2+(y1-y2)**2)**0.5
+def choco(xa,ya,xb,yb,xc,yc,xd,yd): return 1 if abs((yb-ya)*(yd-yc)+(xb-xa)*(xd-xc))<1.e-10 else 0
+
+n,m=map(int,raw_input().split())
+N=range(n)
+w=raw_input()
+for i in range(m):
+    x,y=map(str,raw_input().split())
+    w=w.replace(x,'#')
+    w=w.replace(y,x)
+    w=w.replace('#',y)
+print w
+ans=chk=0
+#end = time.clock()
+#print end - start
