225f48423537c82177b108db04f0e31cba4737eb
nyon <clarinet758@gmail.com>
2016-09-11 16:14:54 +0900

    unrated

---
+++ round/round0351-0375/round0370/b1.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+import time
+import sys
+import io
+import re
+import math
+import itertools
+import collections
+import bisect
+#sys.stdin=file('input.txt')
+#sys.stdout=file('output.txt','w')
+#10**9+7
+mod=1000000007
+#mod=1777777777
+pi=3.141592653589
+IS=float('inf')
+xy=[(1,0),(-1,0),(0,1),(0,-1)]
+bs=[(-1,-1),(-1,1),(1,1),(1,-1)]
+def niten(a,b): return abs(a-b) if a>=0 and b>=0 else  a+abs(b) if a>=0 else abs(a)+b if b>=0 else abs(abs(a)-abs(b))
+def gcd(a,b): return a if b==0 else gcd(b,a%b)
+def lcm(a,b): return a*b/gcd(a,b)
+def euclid_dis(x1,y1,x2,y2): return ((x1-x2)**2+(y1-y2)**2)**0.5
+def choco(xa,ya,xb,yb,xc,yc,xd,yd): return 1 if abs((yb-ya)*(yd-yc)+(xb-xa)*(xd-xc))<1.e-10 else 0
+
+s=raw_input()
+if len(s)%2:
+    print -1
+    exit()
+#URDL
+x,y=0,0
+l=[0]*4
+for i in s:
+    if i=='U':
+        l[0]+=1
+        y+=1
+    elif i=='R':
+        l[1]+=1
+        x+=1
+    elif i=='D':
+        l[2]+=1
+        y-=1
+    else:
+        l[3]+=1
+        x-=1
+if x==y==0:
+    print 0
+else:
+    x=abs(x)
+    y=abs(y)
+    print (x+y)/2
+    pass
+n,k=map(int,raw_input().split())
+l=map(int,raw_input().split())
+ans=chk=0
+#end = time.clock()
+#print end - start
