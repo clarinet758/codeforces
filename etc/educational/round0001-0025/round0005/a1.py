c4aef0e88343d719d19cde0b9243ca6b04f891ed
nyon <clarinet758@gmail.com>
2016-09-19 02:28:20 +0900

    input

---
+++ etc/educational/round0001-0025/round0005/a1.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+import time
+import sys
+import io
+import re
+import math
+import itertools
+import collections
+import bisect
+#sys.stdin=file('input.txt')
+#sys.stdout=file('output.txt','w')
+#10**9+7
+mod=1000000007
+#mod=1777777777
+pi=3.141592653589
+IS=float('inf')
+xy=[(1,0),(-1,0),(0,1),(0,-1)]
+bs=[(-1,-1),(-1,1),(1,1),(1,-1)]
+def niten(a,b): return abs(a-b) if a>=0 and b>=0 else  a+abs(b) if a>=0 else abs(a)+b if b>=0 else abs(abs(a)-abs(b))
+def gcd(a,b): return a if b==0 else gcd(b,a%b)
+def lcm(a,b): return a*b/gcd(a,b)
+def euclid_dis(x1,y1,x2,y2): return ((x1-x2)**2+(y1-y2)**2)**0.5
+def choco(xa,ya,xb,yb,xc,yc,xd,yd): return 1 if abs((yb-ya)*(yd-yc)+(xb-xa)*(xd-xc))<1.e-10 else 0
+
+a=raw_input().lstrip('0')
+b=raw_input().lstrip('0')
+if len(a)>len(b):
+    print '>'
+elif len(a)<len(b):
+    print '<'
+else:
+    if a==b:
+        print '='
+    for i,j in enumerate(a):
+        if a[i]>b[i]:
+            print '>'
+            break
+        elif a[i]<b[i]:
+            print '<'
+            break
+exit()
+n,k=map(int,raw_input().split())
+l=map(int,raw_input().split())
+ans=chk=0
+#end = time.clock()
+#print end - start
