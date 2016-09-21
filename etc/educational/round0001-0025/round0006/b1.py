d95fe0795572da550baaf7067e46ae5a6911172b
nyon <clarinet758@gmail.com>
2016-09-22 03:45:17 +0900

    PyPy2

---
+++ etc/educational/round0001-0025/round0006/b1.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+import time
+import sys
+import io
+import re
+import math
+import itertools
+import collections
+import bisect
+#sys.stdin=file('input.txt')
+#sys.stdout=file('output.txt','w')
+#10**9+7
+mod=1000000007
+#mod=1777777777
+pi=3.141592653589
+IS=float('inf')
+xy=[(1,0),(-1,0),(0,1),(0,-1)]
+bs=[(-1,-1),(-1,1),(1,1),(1,-1)]
+def niten(a,b): return abs(a-b) if a>=0 and b>=0 else  a+abs(b) if a>=0 else abs(a)+b if b>=0 else abs(abs(a)-abs(b))
+def gcd(a,b): return a if b==0 else gcd(b,a%b)
+def lcm(a,b): return a*b/gcd(a,b)
+def euclid_dis(x1,y1,x2,y2): return ((x1-x2)**2+(y1-y2)**2)**0.5
+def choco(xa,ya,xb,yb,xc,yc,xd,yd): return 1 if abs((yb-ya)*(yd-yc)+(xb-xa)*(xd-xc))<1.e-10 else 0
+#PyPy2
+
+l=[6,2,5,5,4,5,6,3,7,6]
+d={}
+for x,i in enumerate(l):
+    d[str(x)]=i
+ans=chk=0
+a,b=map(int,raw_input().split())
+for i in range(a,b+1):
+    for j in str(i):
+        ans+=d[j]
+print ans
+#end = time.clock()
+#print end - start
