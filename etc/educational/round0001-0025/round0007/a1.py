7909722482ea7bb08e09c1b34941786dcc541480
nyon <clarinet758@gmail.com>
2016-10-03 00:05:26 +0900

    syakyo

---
+++ etc/educational/round0001-0025/round0007/a1.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+import time
+import sys
+import io
+import re
+import math
+import itertools
+import collections
+import bisect
+#sys.stdin=file('input.txt')
+#sys.stdout=file('output.txt','w')
+#10**9+7
+mod=1000000007
+#mod=1777777777
+pi=3.141592653589
+IS=float('inf')
+xy=[(1,0),(-1,0),(0,1),(0,-1)]
+bs=[(-1,-1),(-1,1),(1,1),(1,-1)]
+def niten(a,b): return abs(a-b) if a>=0 and b>=0 else  a+abs(b) if a>=0 else abs(a)+b if b>=0 else abs(abs(a)-abs(b))
+def gcd(a,b): return a if b==0 else gcd(b,a%b)
+def lcm(a,b): return a*b/gcd(a,b)
+def euclid_dis(x1,y1,x2,y2): return ((x1-x2)**2+(y1-y2)**2)**0.5
+def choco(xa,ya,xb,yb,xc,yc,xd,yd): return 1 if abs((yb-ya)*(yd-yc)+(xb-xa)*(xd-xc))<1.e-10 else 0
+
+#n,k=map(int,raw_input().split())
+#l=map(int,raw_input().split())
+n=int(raw_input())
+x=int((-1+(8*n)**0.5)/2)
+ans=chk=0
+if x*(x+1)/2==n:
+    print x
+else:
+    print n-x*(x+1)/2
+#end = time.clock()
+#print end - start
