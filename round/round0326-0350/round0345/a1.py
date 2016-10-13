1914063da06f65f0277d91a79434700db6d041bc
nyon <clarinet758@gmail.com>
2016-10-14 00:14:11 +0900

    最近は急に寒くなった

---
+++ round/round0326-0350/round0345/a1.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+import sys
+import re
+import math
+import itertools
+import collections
+import bisect
+#sys.stdin=file('input.txt')
+#sys.stdout=file('output.txt','w')
+#10**9+7
+mod=1000000007
+#mod=1777777777
+pi=3.141592653589
+IS=float('inf')
+xy=[(1,0),(-1,0),(0,1),(0,-1)]
+bs=[(-1,-1),(-1,1),(1,1),(1,-1)]
+def niten(a,b): return abs(a-b) if a>=0 and b>=0 else  a+abs(b) if a>=0 else abs(a)+b if b>=0 else abs(abs(a)-abs(b))
+def fib(n): return [(seq.append(seq[i-2] + seq[i-1]), seq[i-2])[1] for seq in [[0, 1]] for i in range(2, n)]
+def gcd(a,b): return a if b==0 else gcd(b,a%b)
+def lcm(a,b): return a*b/gcd(a,b)
+def eucl(x1,y1,x2,y2): return ((x1-x2)**2+(y1-y2)**2)**0.5
+def choco(xa,ya,xb,yb,xc,yc,xd,yd): return 1 if abs((yb-ya)*(yd-yc)+(xb-xa)*(xd-xc))<1.e-10 else 0
+def pscl(num,l=[1]):
+    for i in range(num):
+        l = map(lambda x,y:x+y,[0]+l,l+[0])
+    return l
+
+#n=int(raw_input())
+#n,k=map(int,raw_input().split())
+l=map(int,raw_input().split())
+ans=chk=0
+while 1:
+    if min(l)<1 or max(l)<2:
+        break
+    if l[0]<=l[1]:
+        l[0]+=1
+        l[1]-=2
+        l[0]=min(l[0],100)
+    else:
+        l[0]-=2
+        l[1]+=1
+        l[1]=min(l[1],100)
+    ans+=1
+print ans    
+#end = time.clock()
+#print end - start
