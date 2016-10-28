140a7b2150ee0fe0546f3140cdaf3171d36d335d
nyon <clarinet758@gmail.com>
2016-10-28 23:47:52 +0900

    風邪ったくさい

--- round/round0326-0350/round0350/a1.py
+++ round/round0326-0350/round0350/a1.py
@@ -20,34 +20,9 @@ def lcm(a,b): return a*b/gcd(a,b)
 def euclid_dis(x1,y1,x2,y2): return ((x1-x2)**2+(y1-y2)**2)**0.5
 def choco(xa,ya,xb,yb,xc,yc,xd,yd): return 1 if abs((yb-ya)*(yd-yc)+(xb-xa)*(xd-xc))<1.e-10 else 0
 
-#n=int(raw_input())
-#n,k=map(int,raw_input().split())
-#l=map(int,raw_input().split())
-#ans=chk=0
-def sol(n):
-    if n%7==0:
-        l=n/7*2
-    elif n%7==6:
-        l=n/7*2+1
-    else:
-        l=n/7
+n=int(raw_input())
+ans=tmp=0
 
-    if n%7>=2:
-        if n>7:
-            r=n/7*2
-        else:
-            r=2
-    elif n%7==1:
-        if n>7:
-            r=n/7*2+1
-        else:
-            r=1
-    else:
-        r=n/7*2
-    return (l,r)
-
-for i in range(1,16):
-    ans=sol(i)
-    print i,ans
+print (n/7)*2+[0,1][n%7==6],(n/7)*2+[0,1,2,2,2,2,2][n%7]
 #end = time.clock()
 #print end - start
