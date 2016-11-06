c93afd546b664fb98a78d2e50884c7c6c5d50db4
nyon <clarinet758@gmail.com>
2016-11-06 12:56:31 +0900

    削除

--- etc/educational/round0001-0025/round0013/a1.py
+++ etc/educational/round0001-0025/round0013/a1.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
-import time
 import sys
-import io
 import re
 import math
 import itertools
@@ -17,20 +15,21 @@ pi=3.141592653589
 IS=float('inf')
 xy=[(1,0),(-1,0),(0,1),(0,-1)]
 bs=[(-1,-1),(-1,1),(1,1),(1,-1)]
+def niten(a,b): return abs(a-b) if a>=0 and b>=0 else  a+abs(b) if a>=0 else abs(a)+b if b>=0 else abs(abs(a)-abs(b))
+def fib(n): return [(seq.append(seq[i-2] + seq[i-1]), seq[i-2])[1] for seq in [[0, 1]] for i in range(2, n)]
 def gcd(a,b): return a if b==0 else gcd(b,a%b)
 def lcm(a,b): return a*b/gcd(a,b)
-def euclid_dis(x1,y1,x2,y2): return ((x1-x2)**2+(y1-y2)**2)**0.5
+def eucl(x1,y1,x2,y2): return ((x1-x2)**2+(y1-y2)**2)**0.5
 def choco(xa,ya,xb,yb,xc,yc,xd,yd): return 1 if abs((yb-ya)*(yd-yc)+(xb-xa)*(xd-xc))<1.e-10 else 0
+def pscl(num,l=[1]):
+    for i in range(num):
+        l = map(lambda x,y:x+y,[0]+l,l+[0])
+    return l
 
-n=int(raw_input())
-for i in range(n):
-    h,b,a=map(str,raw_input().split())
-    if int(b)>=2400 and int(b)<int(a):
-        print 'YES'
-        exit()
-print 'NO'
-exit()
-l=map(int,raw_input().split())
+#n=int(raw_input())
+n,k=map(int,raw_input().split())
+print n-n%k+k
+#l=map(int,raw_input().split())
 ans=chk=0
 #end = time.clock()
 #print end - start
