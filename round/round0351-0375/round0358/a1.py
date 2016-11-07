313b456641dd590a39d50b755959be599e5e8ee8
nyon <clarinet758@gmail.com>
2016-11-07 22:43:18 +0900

    mae

---
+++ round/round0351-0375/round0358/a1.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+import time
+import sys
+import io
+import re
+import math
+import itertools
+import collections
+import bisect
+#sys.stdin=file('input.txt')
+#sys.stdout=file('output.txt','w')
+#10**9+7
+mod=1000000007
+#mod=1777777777
+pi=3.141592653589
+IS=float('inf')
+xy=[(1,0),(-1,0),(0,1),(0,-1)]
+bs=[(-1,-1),(-1,1),(1,1),(1,-1)]
+def gcd(a,b): return a if b==0 else gcd(b,a%b)
+def lcm(a,b): return a*b/gcd(a,b)
+def euclid_dis(x1,y1,x2,y2): return ((x1-x2)**2+(y1-y2)**2)**0.5
+def choco(xa,ya,xb,yb,xc,yc,xd,yd): return 1 if abs((yb-ya)*(yd-yc)+(xb-xa)*(xd-xc))<1.e-10 else 0
+
+#n=int(raw_input())
+n,m=map(int,raw_input().split())
+ans=chk=0
+for i in range(1,n+1):
+    ans+=(i+m)/5-i/5
+print ans
+#l=map(int,raw_input().split())
+#end = time.clock()
+#print end - start
