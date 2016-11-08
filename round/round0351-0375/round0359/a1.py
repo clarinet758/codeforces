878a0368b694ec01b27ac3afe2f688502b7845f7
nyon <clarinet758@gmail.com>
2016-11-09 00:28:54 +0900

    B難しすぎでは。。

--- round/round0351-0375/round0359/a1.py
+++ round/round0351-0375/round0359/a1.py
@@ -22,21 +22,19 @@ def lcm(a,b): return a*b/gcd(a,b)
 def euclid_dis(x1,y1,x2,y2): return ((x1-x2)**2+(y1-y2)**2)**0.5
 def choco(xa,ya,xb,yb,xc,yc,xd,yd): return 1 if abs((yb-ya)*(yd-yc)+(xb-xa)*(xd-xc))<1.e-10 else 0
 
-#n=int(raw_input())
 n,x=map(int,raw_input().split())
-chk=0
+ans=chk=0
 ans=x
 for i in range(n):
     o,d=map(str,raw_input().split())
     tmp=int(d)
     if o=='+':
         ans+=tmp
-    elif o=='-' and ans>=tmp:
-        ans-=tmp
     else:
-        chk+=1
+        if tmp>ans:
+            chk+=1
+        else:
+            ans-=tmp
 print ans,chk
-
-#l=map(int,raw_input().split())
 #end = time.clock()
 #print end - start
