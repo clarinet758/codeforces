8c3f996c7177cc2cb3ae5fe79079324b9f55406d
nyon <clarinet758@gmail.com>
2016-11-22 02:02:15 +0900

    覚え悪すぎ・・

--- round/round0351-0375/round0371/a1.py
+++ round/round0351-0375/round0371/a1.py
@@ -1,39 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
-import time
-import sys
-import io
-import re
-import math
-import itertools
-import collections
-import bisect
-#sys.stdin=file('input.txt')
-#sys.stdout=file('output.txt','w')
-#10**9+7
-mod=1000000007
-#mod=1777777777
-pi=3.141592653589
-IS=float('inf')
-xy=[(1,0),(-1,0),(0,1),(0,-1)]
-bs=[(-1,-1),(-1,1),(1,1),(1,-1)]
-def niten(a,b): return abs(a-b) if a>=0 and b>=0 else  a+abs(b) if a>=0 else abs(a)+b if b>=0 else abs(abs(a)-abs(b))
-def gcd(a,b): return a if b==0 else gcd(b,a%b)
-def lcm(a,b): return a*b/gcd(a,b)
-def euclid_dis(x1,y1,x2,y2): return ((x1-x2)**2+(y1-y2)**2)**0.5
-def choco(xa,ya,xb,yb,xc,yc,xd,yd): return 1 if abs((yb-ya)*(yd-yc)+(xb-xa)*(xd-xc))<1.e-10 else 0
 
-#n=int(raw_input())
-#n,k=map(int,raw_input().split())
 l1,r1,l2,r2,k=map(int,raw_input().split())
-ans=chk=0
-tmp=[l1,r1,l2,r2]
-tmp.sort()
-if tmp[1]>=l1 and tmp[2]<=r1:
-    ans=tmp[2]-tmp[1]
-    if not tmp[1]<=k<=tmp[2]:
-        ans+=1
-print ans
-
-#end = time.clock()
-#print end - start
+s=max(l1,l2)
+g=min(r1,r2)
+print g-s if g>=s and s<=k<=g else g-s+1 if g>=s else 0
