c78d9a002914d0069f4d46c97015776cc2e4c02a
nyon <clarinet758@gmail.com>
2016-11-25 00:39:12 +0900

    まじですげー寒い

--- round/round0351-0375/round0373/a1.py
+++ round/round0351-0375/round0373/a1.py
@@ -1,45 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
-import time
-import sys
-import io
-import re
-import math
-import itertools
-import collections
-import bisect
-#sys.stdin=file('input.txt')
-#sys.stdout=file('output.txt','w')
-#10**9+7
-mod=1000000007
-#mod=1777777777
-pi=3.141592653589
-IS=float('inf')
-xy=[(1,0),(-1,0),(0,1),(0,-1)]
-bs=[(-1,-1),(-1,1),(1,1),(1,-1)]
-def niten(a,b): return abs(a-b) if a>=0 and b>=0 else  a+abs(b) if a>=0 else abs(a)+b if b>=0 else abs(abs(a)-abs(b))
-def gcd(a,b): return a if b==0 else gcd(b,a%b)
-def lcm(a,b): return a*b/gcd(a,b)
-def euclid_dis(x1,y1,x2,y2): return ((x1-x2)**2+(y1-y2)**2)**0.5
-def choco(xa,ya,xb,yb,xc,yc,xd,yd): return 1 if abs((yb-ya)*(yd-yc)+(xb-xa)*(xd-xc))<1.e-10 else 0
 
 n=int(raw_input())
-#n,k=map(int,raw_input().split())
-a=map(int,raw_input().split())
-if len(a)==1:
-    if a[0]==15:
-        print 'DOWN'
-    elif a[0]==0:
-        print 'UP'
-    else:
-        print -1
-elif a[-2]>a[-1] and a[-2]!=1:
+l=map(int,raw_input().split())
+
+if l[-1]==15 or (n>1 and l[-1]!=0 and l[-2]-l[-1]>0):
     print 'DOWN'
+elif l[-1]==0 or (n>1 and l[-2]-l[-1]<0):
+    print 'UP'
 else:
-    if a[-2]==14:
-        print 'DOWN'
-    else:
-        print 'UP'
-ans=chk=0
-#end = time.clock()
-#print end - start
+    print -1
--- round/round0351-0375/round0373/b1.py
+++ round/round0351-0375/round0373/b1.py
@@ -23,39 +23,15 @@ def lcm(a,b): return a*b/gcd(a,b)
 def euclid_dis(x1,y1,x2,y2): return ((x1-x2)**2+(y1-y2)**2)**0.5
 def choco(xa,ya,xb,yb,xc,yc,xd,yd): return 1 if abs((yb-ya)*(yd-yc)+(xb-xa)*(xd-xc))<1.e-10 else 0
 
-n=int(raw_input())
+n=input()
 s=raw_input()
-#n,k=map(int,raw_input().split())
-#l=map(int,raw_input().split())
-t1=t2=t3=t4=0
-if n%2 or n%2==0:
-    for a,i in enumerate(s):
-        if a%2 and i=='r':
-            t2+=1
-        elif a%2==0 and i=='b':
-            t1+=1
-    chk=abs(t2-t1)+min(t2,t1)
-    for a,i in enumerate(s):
-        if a%2 and i=='b':
-            t3+=1
-        elif a%2==0 and i=='r':
-            t4+=1
-    print min(chk,abs(t3-t4)+min(t3,t4))
-else:
-    for a,i in enumerate(s):
-        if (a%2 and a<=n/2-1 and i=='r') or (a==n/2 and i=='r') or (a%2==0 and a>n/2 and i=='r'):
-            t2+=1
-        elif (a%2==0 and a<=n/2 and i=='b') or (a%2==1 and a>n/2 and i=='b'):
-            t1+=1
-    chk=abs(t2-t1)+min(t2,t1)
-    for a,i in enumerate(s):
-        if (a%2 and a<=n/2-1 and i=='b') or (a==n/2 and i=='b') or (a%2==0 and a>n/2 and i=='b'):
-            t3+=1
-        elif (a%2==0 and a<=n/2 and i=='r') or (a%2==1 and a>n/2 and i=='r'):
-            t4+=1
-    print min(chk,abs(t3-t4)+min(t3,t4))
+r1=r2=b1=b2=0
+for a,i in enumerate(s):
+    if a%2==0:
+        if i=='r': r1+=1
+        if i=='b': b2+=1
+    elif a%2==1:
+        if i=='r': r2+=1
+        if i=='b': b1+=1
+print min(min(r1,b1)+abs(r1-b1),min(r2,b2)+abs(r2-b2))
 
-
-
-#end = time.clock()
-#print end - start
