3252aef5acb253b102651d7a457c617a2a0781fa
nyon <clarinet758@gmail.com>
2016-11-27 22:47:19 +0900

    貪欲で多分出来た

--- round/round0376-0400/round0378/b1.py
+++ round/round0376-0400/round0378/b1.py
@@ -1,49 +1,19 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
-import sys
-import re
-import math
-import itertools
-import collections
-import bisect
-#sys.stdin=file('input.txt')
-#sys.stdout=file('output.txt','w')
-#10**9+7
-mod=1000000007
-#mod=1777777777
-pi=3.141592653589
-IS=float('inf')
-xy=[(1,0),(-1,0),(0,1),(0,-1)]
-bs=[(-1,-1),(-1,1),(1,1),(1,-1)]
-def niten(a,b): return abs(a-b) if a>=0 and b>=0 else  a+abs(b) if a>=0 else abs(a)+b if b>=0 else abs(abs(a)-abs(b))
-def fib(n): return [(seq.append(seq[i-2] + seq[i-1]), seq[i-2])[1] for seq in [[0, 1]] for i in range(2, n)]
-def gcd(a,b): return a if b==0 else gcd(b,a%b)
-def lcm(a,b): return a*b/gcd(a,b)
-def eucl(x1,y1,x2,y2): return ((x1-x2)**2+(y1-y2)**2)**0.5
-def choco(xa,ya,xb,yb,xc,yc,xd,yd): return 1 if abs((yb-ya)*(yd-yc)+(xb-xa)*(xd-xc))<1.e-10 else 0
-def pscl(num,l=[1]):
-    for i in range(num):
-        l = map(lambda x,y:x+y,[0]+l,l+[0])
-    return l
 
 n=int(raw_input())
-ans=chk=tmp=0
-l=r=0
-a,b=[0]*n,[0]*n
-
+a=[]
+l=r=ans=chk=0
+p,q=[0]*3,[0]*3
 for i in range(n):
     x,y=map(int,raw_input().split())
-    a[i],b[i]=x,y
-    l+=x
-    r+=y
-ans=abs(l-r)
-for i in range(n):
-    p=l-a[i]+b[i]
-    q=r-b[i]+a[i]
-    if ans<abs(p-q):
-        ans=abs(p-q)
-        chk=i+1
-print chk
-#l=map(int,raw_input().split())
-#end = time.clock()
-#print end - start
+    l,r=l+x,r+y
+    if x>y:
+        if x-y>p[0]-p[1]:
+            p=[x,y,i+1]
+    if x<y:
+        if y-x>q[1]-q[0]:
+            q=[x,y,i+1]
+ll=abs((l-q[0]+q[1])-(r-q[1]+q[0]))
+rr=abs((l-p[0]+p[1])-(r-p[1]+p[0]))
+print 0 if abs(l-r)==max(abs(l-r),ll,rr) else q[2] if ll>rr else p[2]
