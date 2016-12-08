b8ee4b1237b5e8d137082681eda5ef94aeaf648d
nyon <clarinet758@gmail.com>
2016-12-08 22:51:13 +0900

    short

--- round/round0176-0200/round0191/b1.py
+++ round/round0176-0200/round0191/b1.py
@@ -1,19 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
-import time
-import sys
-import io
-import re
-import math
-import itertools
-#sys.stdin=file('input.txt')
-#sys.stdout=file('output.txt','w')
-#10**9+7
+
 mod=1000000007
-#start = time.clock()
 n=input()
 m=10000001
 ans=xrange(m-n,m)
 print ' '.join(map(str,ans))
-#end = time.clock()
-#print end - start
