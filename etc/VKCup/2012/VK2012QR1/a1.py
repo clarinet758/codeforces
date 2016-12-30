7417d65f4277f9f4d8d2102d19a5a38732297f14
nyon <clarinet758@gmail.com>
2016-12-30 21:10:19 +0900

    勘違いだった

--- etc/VKCup/2012/VK2012QR1/a1.py
+++ etc/VKCup/2012/VK2012QR1/a1.py
@@ -1,12 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
-import time
-import sys
-import io
-import re
-import math
-import itertools
-#start = time.clock()
+
 n,k=map(int,raw_input().split())
 l=[int(x) for x in raw_input().split()]
 ans=chk=0
@@ -14,7 +8,3 @@ for i in range(n):
     if l[i]>=l[k-1] and l[i]>0:
         ans+=1
 print ans
-
-
-#end = time.clock()
-#print end - start
