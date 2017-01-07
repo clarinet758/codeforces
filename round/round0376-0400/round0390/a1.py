887957ab3d072583e8b761c8fb7ee79eb2a1351e
nyon <clarinet758@gmail.com>
2017-01-07 17:27:40 +0900

    seiri

--- round/round0376-0400/round0390/a1.py
+++ round/round0376-0400/round0390/a1.py
@@ -1,47 +1,26 @@
 #!/usr/bin/env pypy3
 # -*- coding: UTF-8 -*-
-import sys
-import re
-import math
-import itertools
-import collections
-import bisect
-#sys.stdin=file('input.txt')
-#sys.stdout=file('output.txt','w')
-#10**9+7
-mod=1000000007
-#mod=1777777777
-pi=3.1415926535897932
-IS=float('inf')
-xy=[(1,0),(-1,0),(0,1),(0,-1)]
-bs=[(-1,-1),(-1,1),(1,1),(1,-1)]
-def niten(a,b): return abs(a-b) if a>=0 and b>=0 else  a+abs(b) if a>=0 else abs(a)+b if b>=0 else abs(abs(a)-abs(b))
-def fib(n): return [(seq.append(seq[i-2] + seq[i-1]), seq[i-2])[1] for seq in [[0, 1]] for i in range(2, n)]
-def gcd(a,b): return a if b==0 else gcd(b,a%b)
-def lcm(a,b): return a*b/gcd(a,b)
-def eucl(x1,y1,x2,y2): return ((x1-x2)**2+(y1-y2)**2)**0.5
-def choco(xa,ya,xb,yb,xc,yc,xd,yd): return 1 if abs((yb-ya)*(yd-yc)+(xb-xa)*(xd-xc))<1.e-10 else 0
-def pscl(num,l=[1]):
-    for i in range(num):
-        l = map(lambda x,y:x+y,[0]+l,l+[0])
-    return l
 
 n=int(input())
-#n,k=map(int,input().split())
 l=[int(i) for i in input().split()]
 ans=chk=0
 if sum(l)==0 and len(l)==1:
     print('NO')
     exit()
 else:
-    for i in range(1,n):
-#        print(l[:i],l[i:])
-        if sum(l[:i])!=0 and sum(l[i:]):
-            print('YES')
-            print(2)
-            print(1,i)
-            print(i+1,n)
-            exit()
-print('NO')
+    if sum(l)==0 and l.count(0)==n:
+        print('NO')
+    elif sum(l)!=0:
+        print('YES')
+        print(1)
+        print(1,n)
 
+    else:
+        for i in range(1,n):
+            if sum(l[:i])!=0 and sum(l[i:]):
+                print('YES')
+                print(2)
+                print(1,i)
+                print(i+1,n)
+                exit()
 
