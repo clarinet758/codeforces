47388e6632d0fdf7f8ffb079ed6e1bd66c6eceb6
nyon <clarinet758@gmail.com>
2017-01-08 01:18:10 +0900

    分岐条件見直し

--- round/round0376-0400/round0390/a1.py
+++ round/round0376-0400/round0390/a1.py
@@ -4,23 +4,19 @@
 n=int(input())
 l=[int(i) for i in input().split()]
 ans=chk=0
-if sum(l)==0 and len(l)==1:
+if l.count(0)==n:
     print('NO')
-    exit()
-else:
-    if sum(l)==0 and l.count(0)==n:
-        print('NO')
-    elif sum(l)!=0:
-        print('YES')
-        print(1)
-        print(1,n)
+elif sum(l):
+    print('YES')
+    print(1)
+    print(1,n)
 
-    else:
-        for i in range(1,n):
-            if sum(l[:i])!=0 and sum(l[i:]):
-                print('YES')
-                print(2)
-                print(1,i)
-                print(i+1,n)
-                exit()
+else:
+    for i in range(1,n):
+        if sum(l[:i]) and sum(l[i:]):
+            print('YES')
+            print(2)
+            print(1,i)
+            print(i+1,n)
+            break
 
