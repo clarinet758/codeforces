cb158ebd8a721692cfc5cd70238a39c6fc08fbbc
nyon <clarinet758@gmail.com>
2017-02-15 00:53:17 +0900

    疲れた

--- round/round0376-0400/round0391/b1.py
+++ round/round0376-0400/round0391/b1.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env pypy3
 # -*- coding: UTF-8 -*-
-import bisect
 
 def prime_t(t):
     i=2
@@ -17,20 +16,33 @@ def prime_list(tt):
             p_list.append(i)
     return p_list
 
-
+#317まで、317*317=100489、、までの素数を用意
+chk=prime_list(318)
+#317までの素数を個別にと、+1で確保したものは適当に使う
+ans=[0]*(len(chk)+1)
+ans[-1]=1
 n=int(input())
-l=[int(i) for i in input().split()]
+#1はいらないので捨てる
+l=[int(i) for i in input().split() if i!='1']
+
 d={}
+#同じ数がそれぞれ何個あるかを連想配列にいれる
+#ans[-1]には最大の個数を入れておく、現時点では解の有力候補
 for i in l:
     if i in d:
         d[i]+=1
+        ans[-1]=max(ans[-1],d[i])
     else:
         d[i]=1
+
+#重複してるものの個数を数え終わったのでsetで重複排除する
 l=list(set(l))
 l.sort()
 
-chk=prime_list(max(l))
-ans=[0]*(len(chk)+1)
+#それぞれの数を317までの素数で割り切れなくなるまで割る
+#317より大きな素数は新たな連想配列に入れて個数を数える
+#317より大きい素数の個数とans[-1]を比較して大きければ更新する
+over={}
 for i in l:
     tmp=i
     for j,k in enumerate(chk):
@@ -40,4 +52,11 @@ for i in l:
                 i//=k
         if i<k:
             break
-print(max(max(ans),1))
+    if i>317:
+        if i in over:
+            over[i]+=d[tmp]
+            ans[-1]=max(ans[-1],over[i])
+        else:
+            over[i]=d[tmp]
+#ansのなかで最大のものが解になる
+print(max(ans))
