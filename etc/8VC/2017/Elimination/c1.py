4d4518992dd5c13e177b940c55aedd3924714e06
nyon <clarinet758@gmail.com>
2017-02-17 22:07:25 +0900

    hooo

--- etc/8VC/2017/Elimination/c1.py
+++ etc/8VC/2017/Elimination/c1.py
@@ -1,25 +1,14 @@
 #!/usr/bin/env pypy3
 # -*- coding: UTF-8 -*-
 
-import heapq
-n=int(input())
+input()
 l=[int(i) for i in input().split()]
-syu={}
-ich=[]
-d={i for i in range(1,n+1)}
-for a,i in enumerate(l):
-    if i in syu:
-        syu[i]+=1
-    else:
-        syu[i]=1
-        heapq.heappush(ich,i)
-while len(ich):
-    tmp=heapq.heappop(ich)
-    if tmp<l[tmp-1]:
-        syu[tmp]+=syu[l[tmp-1]]
-        syu[l[tmp-1]]=0
 ans=0
-for i in syu:
-    if syu[i]>0:
+w=set([])
+for a,i in enumerate(l):
+    if a+1==i:
         ans+=1
-print(ans)
+    else:
+        w.add(i)
+print(ans+len(w)//2)
+
