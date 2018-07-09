556b53d703b9e590d31ba8c5cb29e08e949567dd
clarinet758 <clarinet758@gmail.com>
2018-07-10 00:28:31 +0900

    hagelu

--- round/round0376-0400/round0399/a1.py
+++ round/round0376-0400/round0399/a1.py
@@ -1,13 +1,20 @@
-#!/usr/bin/env pypy3
+#!/usr/bin/env python3
 # -*- coding: UTF-8 -*-
-
 n=int(input())
-l=[int(i) for i in input().split()]
+a=[int(i) for i in input().split()]
 d={}
-for i in l:
+ans=0
+for i in a:
     if i in d:
         d[i]+=1
     else:
         d[i]=1
-s=set(l)
-print(max(n-d[min(s)]-d[max(s)],0))
+a=list(set(a))
+a.sort()
+if len(a)<3:
+    print(ans)
+else:
+    for i in range(1,len(a)-1):
+        ans+=d[a[i]]
+    print(ans)
+
